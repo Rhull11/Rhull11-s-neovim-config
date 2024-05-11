e026457744fba25a946c5a148180b9b74d821f46
rhull <rhull9@outlook.com>
2024-05-11 00:17:33 -0600

    adding mypy

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,37 +2,42 @@ return {
     {
         "williamboman/mason.nvim",
         config = function()
-            require("mason").setup()
-        end
+            require("mason").setup({
+                ensure_installed = {"mypy", "black"}
+            })
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {}),
-                ensure_installed = { "lua_ls", "ruff_lsp" }
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {}),
+                ensure_installed = { "lua_ls", "pyright", "ruff_lsp"},
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
             })
             lspconfig.ruff_lsp.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-            vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, {})
-        end
-    }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+            vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, {})
+        end,
+    },
 }
